b598eab15e216ea4c6eab210466d50f33b500159
Mykhailo Forkosh <forkos277@gmail.com>
2024-12-07 13:28:54 +0200

    v.0.4

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,49 +1,19 @@
 'use strict';
 
-const allElementSpan = [...document.querySelectorAll('span')];
-const numbersFromSpan = [];
-
-for (const ch of allElementSpan) {
-  if (parseInt(ch.innerText)) {
-    const stringWithoutComma = ch.innerText.replaceAll(',', '');
-    const parseToInt = parseInt(stringWithoutComma);
-
-    numbersFromSpan.push(parseToInt);
-  }
-}
-
+const populationSpan = [...document.querySelectorAll('.population')];
 let totalNum = 0;
 
-for (const num of numbersFromSpan) {
-  totalNum += num;
+for (const ch of populationSpan) {
+  const stringWithoutComma = ch.innerText.replaceAll(',', '');
+  const parseToInt = parseInt(stringWithoutComma);
+
+  totalNum += parseToInt;
 }
 
-let averageNum = Math.round(totalNum / numbersFromSpan.length);
+const averageNum = Math.round(totalNum / populationSpan.length);
 
-averageNum = numberToDigit(averageNum);
-totalNum = numberToDigit(totalNum);
+document.querySelector('.total-population').outerHTML =
+  totalNum.toLocaleString('en-GB');
 
-const totalNumSpanElement = document.querySelector('.total-population');
-const averageNumSpanElement = document.querySelector('.average-population');
-
-function numberToDigit(num) {
-  const stringOfNum = String(num);
-  const arrayNumber = [];
-  let digit = '';
-
-  for (let i = stringOfNum.length - 1; i >= 0; i--) {
-    digit = stringOfNum[i] + digit;
-
-    if (digit.length === 3) {
-      arrayNumber.unshift(digit);
-      digit = '';
-    } else if (i === 0) {
-      arrayNumber.unshift(digit);
-    }
-  }
-
-  return arrayNumber.join(',');
-}
-
-totalNumSpanElement.outerHTML = totalNum;
-averageNumSpanElement.outerHTML = averageNum;
+document.querySelector('.average-population').outerHTML =
+  averageNum.toLocaleString('en-GB');
