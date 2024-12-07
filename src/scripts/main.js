c08b5345e61f3cb02e4caed81093ac5ecf4f958f
Mykhailo Forkosh <forkos277@gmail.com>
2024-12-07 12:49:27 +0200

    v.0.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,49 @@
 'use strict';
 
-// write your code here
+const allElementSpan = [...document.querySelectorAll('span')];
+const numbersFromSpan = [];
+
+for (const ch of allElementSpan) {
+  if (parseInt(ch.innerText)) {
+    const stringWithoutComma = ch.innerText.replaceAll(',', '');
+    const parseToInt = parseInt(stringWithoutComma);
+
+    numbersFromSpan.push(parseToInt);
+  }
+}
+
+let totalNum = 0;
+
+for (const num of numbersFromSpan) {
+  totalNum += num;
+}
+
+let averageNum = Math.round(totalNum / numbersFromSpan.length);
+
+averageNum = numberToDigit(averageNum);
+totalNum = numberToDigit(totalNum);
+
+const totalNumSpanElement = document.querySelector('.total-population');
+const averageNumSpanElement = document.querySelector('.average-population');
+
+function numberToDigit(num) {
+  const stringOfNum = String(num);
+  const arrayNumber = [];
+  let digit = '';
+
+  for (let i = stringOfNum.length - 1; i >= 0; i--) {
+    digit = stringOfNum[i] + digit;
+
+    if (digit.length === 3) {
+      arrayNumber.unshift(digit);
+      digit = '';
+    } else if (i === 0) {
+      arrayNumber.unshift(digit);
+    }
+  }
+
+  return arrayNumber.join(',');
+}
+
+totalNumSpanElement.outerHTML = totalNum;
+averageNumSpanElement.outerHTML = averageNum;
